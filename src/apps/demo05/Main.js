ae8885bd55291d5f63fa985a0279ee557019b5f1
Matteo Nastasi <nastasi@alternativeoutput.it>
2019-08-29 10:55:28 +0200

    move components references to Board component

--- src/apps/demo05/Main.js
+++ src/apps/demo05/Main.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './Main.css';
 
+function print(arg) {
+  console.log(arg);
+}
+
 // SitDown
 // WakeUp
 // ChangeColor
@@ -26,37 +30,6 @@ class User extends React.PureComponent {
 }
 
 class Table extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.users_comp = [React.createRef(),
-                       React.createRef(),
-                       React.createRef(),
-                       React.createRef(),
-                       React.createRef()];
-
-    this.handleClick = this.handleClick.bind(this);
-    this.setUser_gen = this.setUser_gen.bind(this);
-  }
-
-  setUser_gen(idx) {
-    let _idx = idx;
-    let _this = this;
-
-    return function(comp) {
-      _this.users_comp[_idx].current = comp;
-    }
-  }
-
-  handleClick() {
-    // this.setState(state => ({
-    //  acceso: !state.acceso
-    // }));
-    console.log("handleClick fired");
-    console.log(this.users_comp);
-    this.users_comp[0].current.setState(
-        function(state, props){ console.log('updater'); console.log(state); return {...state, color: "#000000"}; });
-  }
-
   render () {
     console.log('Table render');
     var style = {paddingLeft: "32px"};
@@ -65,35 +38,17 @@ class Table extends React.PureComponent {
         <h2>Table {this.props.name}</h2>
         <div>
         {this.props.users.map(function (user, id) {
-          return (<User key={user.id} name={user.name} color={user.color}  ref={this.setUser_gen(user.pos)}/>);
+          return (<User key={user.id} name={user.name} color={user.color}
+                  ref={this.props.setUser_gen(user.id)}/>);
         }, this)}
       </div>
-        <button onClick={this.handleClick}>Clean</button>
+        <button onClick={this.props.handleTableClick}>Clean</button>
       </div>
     );
   }
 }
 
 class Standup extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.users_comp = [React.createRef(),
-                       React.createRef(),
-                       React.createRef(),
-                       React.createRef(),
-                       React.createRef()];
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    // this.setState(state => ({
-    //  acceso: !state.acceso
-    // }));
-    console.log("handleClick fired");
-    this.users_comp[0].current.setState(
-      function(state, props){ console.log('updater'); console.log(state); return {...state, color: "#000000"}; });
-  }
-
   render () {
     console.log('Standup render');
     return (
@@ -101,10 +56,11 @@ class Standup extends React.PureComponent {
         <h2>Standup</h2>
         <div>
       {this.props.users.map((user, id) => (
-          <User key={user.id} name={user.name} color={user.color} ref={this.users_comp[user.pos]}/>
+          <User key={user.id} name={user.name} color={user.color}
+        ref={this.props.setUser_gen(user.id)}/>
       ))}
       </div>
-        <button onClick={this.handleClick}>Clean</button>
+        <button onClick={this.props.handleTableClick}>Clean</button>
         </div>
     );
   }
@@ -114,16 +70,49 @@ class Standup extends React.PureComponent {
 class Board extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.tables = [{id: 0, name: "Zer"},
-                   {id: 1, name: "Uno"},
-                   {id: 2, name: "Due"},
-                   {id: 3, name: "Tre"}];
-    this.users = [{id:0, name: "Pippo", color: "#ff0000", table: null, pos: 0},
-                  {id:1, name: "Pluto", color: "#00ff00", table: "1", pos: 1},
-                  {id:2, name: "Paperino", color: "#0000ff", table: "1", pos: 0},
-                  {id:3, name: "Minnie", color: "#ff00ff", table: 2, pos: 0},
-                  {id:4, name: "Gastone", color: "#ffff00", table: null, pos: 1}
+    this.tables = [{id: 0, name: "Zer", comp: React.createRef()},
+                   {id: 1, name: "Uno", comp: React.createRef()},
+                   {id: 2, name: "Due", comp: React.createRef()},
+                   {id: 3, name: "Tre", comp: React.createRef()}];
+    this.users = [{id:0, name: "Pippo", color: "#ff0000", table: null, pos: 0, comp: React.createRef()},
+                  {id:1, name: "Pluto", color: "#00ff00", table: 1, pos: 1, comp: React.createRef()},
+                  {id:2, name: "Paperino", color: "#0000ff", table: 1, pos: 0, comp: React.createRef()},
+                  {id:3, name: "Minnie", color: "#ff00ff", table: 2, pos: 0, comp: React.createRef()},
+                  {id:4, name: "Gastone", color: "#ffff00", table: null, pos: 1, comp: React.createRef()}
                  ];
+    this.setUser_gen = this.setUser_gen.bind(this);
+    this.handleTableClick_gen = this.handleTableClick_gen.bind(this);
+  }
+
+  setUser_gen() {
+    let _this = this;
+
+    return function(user_idx) {
+      let _user_idx = user_idx;
+
+      console.log('first');
+      console.log(_user_idx);
+
+      return function(comp) {
+        console.log('second');
+        console.log(_this);
+        _this.users[_user_idx].comp.current = comp;
+      }
+    }
+  }
+
+  handleTableClick_gen(table_idx) {
+    let _this = this;
+    let _table_idx = table_idx;
+
+    return function() {
+      console.log("handleTableClick fired " + _table_idx);
+      var users = _this.users.filter((item) => (item.table == _table_idx));
+      users.sort((a, b) => (a.pos > b.pos));
+      console.log(users);
+      users[0].comp.current.setState(
+        function(state, props){ console.log('updater'); console.log(state); return {...state, color: "#000000"}; });
+    }
   }
 
   render () {
@@ -137,10 +126,15 @@ class Board extends React.PureComponent {
           function(table, id, arr) {
             var users = this.users.filter((item) => (item.table == id));
             users.sort((a, b) => (a.pos > b.pos));
-            return (<Table key={id} id={id} name={table.name} users={users}/>);
+            return (<Table key={id} id={id} name={table.name} users={users}
+                    setUser_gen={this.setUser_gen()}
+                    handleTableClick={this.handleTableClick_gen(id)}/>);
           }, this)}
-        <Standup users={this.users.filter((item) => (item.table == null))} />
-        </div>
+        <Standup users={this.users.filter((item) => (item.table == null))}
+                 setUser_gen={this.setUser_gen()}
+                 handleTableClick={this.handleTableClick_gen(null)}/>
+
+      </div>
       </div>
     );
   }
